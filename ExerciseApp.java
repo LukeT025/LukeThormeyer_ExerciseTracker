86e05388b6960e801bd7e54e18aa0945eebb0d47
Luke Thormeyer <lukemthormeyer@lewisu.edu>
2023-11-07 14:59:01 -0600

    first upload of code

---
+++ ExerciseApp.java
@@ -0,0 +1,13 @@
+
+public class ExerciseApp {
+	public static void printWelcomeBanner(){
+		System.out.println("*".repeat(72));
+		System.out.println(" ".repeat(28) + "Exercise Tracker");
+		System.out.println("*".repeat(72));
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		printWelcomeBanner();
+	}
+}
